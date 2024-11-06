4f5b2d47094dc19953a450ed9241031a1a529fd9
Tim Perry <pimterry@gmail.com>
2024-11-06 18:24:40 +0100

    Remove duplicate messages

--- check.js
+++ check.js
@@ -8,10 +8,8 @@ exports.isMsvcRuntimeInstalled = async function () {
 
     try {
         await execAsync('where vcruntime140.dll');
-        console.log('Visual C++ Redistributable is installed.');
         return true;
     } catch (error) {
-        console.error('Visual C++ Redistributable is not installed. Please install it from https://aka.ms/vs/16/release/vc_redist.x64.exe');
         return false;
     }
 };
